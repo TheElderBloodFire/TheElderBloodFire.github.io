9763bea550a7a2eb88293d3deb142afe0c651927
AtakanFire <atakanates06@gmail.com>
2018-08-16 19:09:54 +0300

    Language Updated!
    Language updated!
    Artworks redesigned!

---
+++ js/LanguageChanger.js
@@ -0,0 +1,47 @@
+var currentLang = "en";
+
+StartLanguage();
+
+function SelectLanguage(language) {
+  currentLang = language;
+  var langs = document.getElementsByName('lang');
+  for (var i = 0; i < langs.length; i++) {
+    langs[i].style.display = "none";
+    if(langs[i].lang == currentLang){
+      //console.log("Language: " + currentLang);
+      langs[i].style.display = "block";
+    }
+    //console.log(i + ": " + langs[i].lang );
+  }
+}
+
+function ChangeLanguage() {
+  if (currentLang == "en") {
+    currentLang = "tr";
+    SelectLanguage(currentLang);
+    ChangeiframeLanguage();
+  }else {
+    currentLang = "en";
+    SelectLanguage(currentLang);
+    ChangeiframeLanguage();
+  }
+}
+
+function ChangeiframeLanguage() {
+  if (document.getElementById("WorkViewer")) {
+    document.getElementById("WorkViewer").contentWindow.ChangeLanguage();    
+  }
+}
+
+function StartLanguage() {
+  var localLang = navigator.language || navigator.userLanguage;
+  if (localLang == "tr-TR") {
+    if (currentLang == "en") {
+      currentLang = "tr";
+      SelectLanguage(currentLang);
+    }else {
+      currentLang = "en";
+      SelectLanguage(currentLang);
+    }
+  }
+}
